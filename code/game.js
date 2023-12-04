02af82d89c545b551fe96f9bf80af12e3e54a9e0
Henry-H08 <143623813+Henry-H08@users.noreply.github.com>
2023-12-04 08:54:21 -0500

    5ygjkr

--- code/game.js
+++ code/game.js
@@ -28,7 +28,7 @@ let background = add([
     pos(width() / 2, height() / 2),
     origin("center"),
     // Allow the background to be scaled
-    scale(1),
+    scale(5),
     // Keep the background position fixed even when the camera moves
     fixed()
   ]);
