cd78e08c6f183a738e2c13f30d7eb6d584ba975e
Henry-H08 <143623813+Henry-H08@users.noreply.github.com>
2023-12-05 10:22:19 -0500

    SEE APPLE ?

--- code/game.js
+++ code/game.js
@@ -1,23 +1,5 @@
-const apple = add([
-	sprite("apple"),   
-	pos(120, 80),           
-	anchor("center"), 
-  	area(),
-])
 
-var rah = 0;
 
-apple.onClick(() => {
-  add([
-    rect(width(), height()/2.5),
-    pos(width()/2, height()),
-    outline(4),
-    area(),
-    anchor("center"),
-    color(BLACK),
-  ])
-	
-})
 
 
 
@@ -31,3 +13,22 @@ add([
     fixed(),
     pos(0, 0)
 ])
+
+const apple = add([
+	sprite("apple"),   
+	pos(120, 80),           
+	anchor("center"), 
+  	area(),
+])
+
+apple.onClick(() => {
+  add([
+    rect(width(), height()/2.5),
+    pos(width()/2, height()),
+    outline(4),
+    area(),
+    anchor("center"),
+    color(BLACK),
+  ])
+	
+})
